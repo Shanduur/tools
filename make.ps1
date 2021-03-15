69ad11f11f2b2b4da3da1b101fc7825e404ef983
Mateusz Urbanek <mateusz.urbanek@asseco.pl>
2021-03-15 08:11:38 +0100

    feat: added install to make script

--- make.ps1
+++ make.ps1
@@ -4,5 +4,8 @@ for ( $i = 0; $i -lt $args.count; $i++ ) {
             go build -o ./build/fmt.exe ./fmt/main.go
             write-host "building $($args[$i]) finished with code $LASTEXITCODE"
         }
+        "install" {
+            Copy-Item -Path ".\build\*.exe" -Destination "C:\Tools" -Force -Verbose
+        }
     } 
 }
